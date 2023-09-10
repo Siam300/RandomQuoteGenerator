3a0a28574853735976fdfdf16b3a8181bee03570
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-09-10 23:42:35 +0600

    Initial Commit

---
+++ quoteGenerator/quoteGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  quoteGeneratorApp.swift
+//  quoteGenerator
+//
+//  Created by Auto on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct quoteGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
