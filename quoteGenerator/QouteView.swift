3e60d62f68ef91777f6bc839665ca7271c56a2ef
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-09-12 21:20:50 +0600

    Randomized background added.

--- quoteGenerator/QouteView.swift
+++ quoteGenerator/QouteView.swift
@@ -13,32 +13,64 @@ extension Array {
     }
 }
 
+struct aPicture: Identifiable {
+    var id: Int
+    var name: String
+    var imageName: String
+}
+
 struct QuoteView: View {
     @StateObject private var viewModel = QuoteViewModel()
+    @State var random : Int = 0
+    
+    let pictures = [
+        aPicture(id: 0, name: "1", imageName: "1"),
+        aPicture(id: 1, name: "2", imageName: "2"),
+        aPicture(id: 2, name: "3", imageName: "3"),
+        aPicture(id: 3, name: "4", imageName: "4"),
+        aPicture(id: 4, name: "5", imageName: "5"),
+        aPicture(id: 5, name: "6", imageName: "6"),
+    ]
     
     var body: some View {
-        VStack {
-            if let quote = viewModel.quotes[safe: viewModel.currentIndex] {
-                Text(quote.content)
-                    .padding()
-                Text("- " + quote.author)
-                    .padding()
-                    .foregroundColor(.gray)
-            }
-        }
-        .gesture(
-            DragGesture(minimumDistance: 50)
-                .onEnded { value in
-                    if value.translation.width < 0 {
-                        viewModel.nextQuote()
-                    } else if value.translation.width > 0 {
-                        viewModel.previousQuote()
-                    }
+        ZStack {
+            Image(pictures[self.random].imageName)
+                .resizable()
+                .scaledToFit()
+                .aspectRatio(contentMode: .fill)
+                .clipped()
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                if let quote = viewModel.quotes[safe: viewModel.currentIndex] {
+                    Text(quote.content)
+                        .frame(maxWidth: 340)
+                        .padding(.all)
+                        .background(Color.white.opacity(0.8))
+                        .multilineTextAlignment(.center)
+                        .padding(.all)
+                    Text("- " + quote.author)
+                        .foregroundColor(.gray)
+                        .fontWeight(.heavy)
+                        .padding(.all)
                 }
-        )
-        .onAppear {
-            if viewModel.quotes.isEmpty {
-                viewModel.getData()
+            }
+            .padding(.all)
+            .gesture(
+                DragGesture(minimumDistance: 50)
+                    .onEnded { value in
+                        if value.translation.width < 0 {
+                            viewModel.nextQuote()
+                            self.random = Int.random(in: 0..<self.pictures.count)
+                        } else if value.translation.width > 0 {
+                            viewModel.previousQuote()
+                            self.random = Int.random(in: 0..<self.pictures.count)
+                        }
+                    }
+            )
+            .onAppear {
+                if viewModel.quotes.isEmpty {
+                    viewModel.getData()
+                }
             }
         }
     }
